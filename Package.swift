fff3fee2c3c4941bc4e505a370699cf6c5754f75
stefanspringer1 <stefan.springer@icloud.com>
2021-08-22 10:54:40 +0200

    using SwiftXML 0.1.5 and using swift-tools-version:5.4.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.4.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
     name: "SwiftXMLDemo",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/stefanspringer1/SwiftXML.git", from: "0.1.3"),
+        .package(url: "https://github.com/stefanspringer1/SwiftXML.git", from: "0.1.5"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
